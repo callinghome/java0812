5fb022d6d691565a9fbe4a5168404a628fdc7847
MB-study <MB-study@192.168.1.113>
2020-08-12 13:36:00 +0800

    java0812

---
+++ src/main/java/com/psschool/java0811/ocp/d02/MultiArray.java
@@ -0,0 +1,14 @@
+
+package com.psschool.java0811.ocp.d02;
+
+public class MultiArray {
+    public static void main(String[] args) {
+        int[][] x = new int[2][3];
+        x[0][0] = 90;
+        System.out.println(x);
+        System.out.println(x[0]);
+        System.out.println(x[0][0]);
+        
+    }
+  
+}
---
+++ src/main/java/com/psschool/java0811/ocp/d02/MultiArray2.java
@@ -0,0 +1,16 @@
+package com.psschool.java0811.ocp.d02;
+
+public class MultiArray2 {
+
+    public static void main(String[] args) {
+        int[][] x = new int[][]{{100, 90, 80}, {70, 60, 50}};//array initialize 
+        //前面的new int[][]通常會拿掉
+        //將x[0]的內容取出
+        for (int j = 0; j < x.length; j++) {
+            for (int i = 0; i < x[0].length; i++) {
+                System.out.printf("%4d",x[j][i]);
+            }
+            System.out.println();
+        }
+    }
+}
